369ff9dbdaf229489a35217d03b3dd132c037d7c
jomorais <jmorais@vtinova.com.br>
2017-05-26 17:28:27 -0400

    c

---
+++ user2.py
@@ -0,0 +1,21 @@
+__author__ = 'jomorais'
+
+from pyfase import MicroService
+
+
+class User2(MicroService):
+    def __init__(self):
+        super(User2, self).__init__(self, sender_endpoint='ipc:///tmp/sender', receiver_endpoint='ipc:///tmp/receiver')
+
+    def on_connect(self):
+        print('### on_connect ###')
+        self.request_action('msg', {'msg': self.__class__.__name__})
+
+    def on_broadcast(self, service, data):
+        print('### on_broadcast ### service: %s - data: %s' % (service, data))
+
+    @MicroService.action
+    def msg(self, service, data):
+        print data
+
+User2().execute()
